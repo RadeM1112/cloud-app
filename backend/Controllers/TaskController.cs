7fdba231e0e89763a0b061a06638186ef8f60f7e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-03-23 18:56:47 +0100

    add contr

--- backend/Controllers/TaskController.cs
+++ backend/Controllers/TaskController.cs
@@ -16,27 +16,58 @@ public class TasksController : ControllerBase
         _context = context;
     }
 
-    [HttpGet]
-    public async Task<ActionResult> GetAll()
-    {
-        return Ok(await _context.Tasks.ToListAsync());
+   [HttpGet]
+   public async Task<ActionResult<IEnumerable<TaskReadDto>>> GetAll()
+   {
+ 
+    	var tasks = await _context.Tasks.ToListAsync();
+    	var tasksDto = tasks.Select(t => new TaskReadDto
+    	{
+        	Id = t.Id,
+        	Name = t.Name,
+        	IsCompleted = t.IsCompleted
+        });
+    	return Ok(tasksDto);
     }
 
+   
     [HttpGet("{id}")]
-    public async Task<ActionResult> GetById(int id)
+    public async Task<ActionResult<TaskReadDto>> GetById(int id)
     {
-        var task = await _context.Tasks.FindAsync(id);
-        return task == null ? NotFound() : Ok(task);
+    	var task = await _context.Tasks.FindAsync(id);
+    	if (task == null) return NotFound();  // Zwracamy DTO zamiast czystej encji
+    	return Ok(new TaskReadDto 
+    	{ 
+        	Id = task.Id, 
+        	Name = task.Name, 
+        	IsCompleted = task.IsCompleted 
+    	});
     }
 
     [HttpPost]
-    public async Task<ActionResult> Create(CloudTask task)
+    public async Task<ActionResult<TaskReadDto>> Create(TaskCreateDto taskDto)
     {
-        _context.Tasks.Add(task);
-        await _context.SaveChangesAsync();
 
-        return CreatedAtAction(nameof(GetById), new { id = task.Id }, task);
+    var newTask = new CloudTask
+    {
+        Name = taskDto.Name,
+        IsCompleted = false // Domyślnie nowe zadanie nie jest gotowe
+    };
+
+
+    _context.Tasks.Add(newTask);
+    await _context.SaveChangesAsync();
+
+    var readDto = new TaskReadDto
+    {
+        Id = newTask.Id,
+        Name = newTask.Name,
+        IsCompleted = newTask.IsCompleted
+    };
+
+    	return CreatedAtAction(nameof(GetById), new { id = readDto.Id }, readDto);
     }
+ 
 
     [HttpPut("{id}")]
     public async Task<ActionResult> Update(int id, CloudTask task)
